3b9cf398e0a0a47cb506b510454a635b65e901fa
Hei-sen-bergg <senrajshekar98@gmail.com>
2024-03-29 22:45:35 +0530

    changed button size and added a function

--- notes.js
+++ notes.js
@@ -14,7 +14,8 @@ checkIcon.addEventListener("click",function(){
 })
 
 function typeNote(){
-    if(container3.style.display == "none"){
+
+    if(container3.style.display == "none"){    //create note & delete button to hide and display add note
         container3.style.display = "block";
     }
     else{
@@ -22,20 +23,20 @@ function typeNote(){
     }
 }
 
-function createNote(){
- var noteText = document.getElementById("note-text").value;
- var element1 = document.createElement('div');
+function createNote(){                        //input text for notes
+    var element1 = document.createElement('div');
+ var noteText = document.getElementById("note-text").value;  
  var element2 = document.createElement('h1');
 
  element2.innerHTML = noteText;
 
-element2.setAttribute("style","width:260px; height:260px; font-size:25px; padding:25px; margin-top:10px; overflow:hidden; box-shadow: 10px 10px 5px 0px rgba(0, 0, 0, 0.4)");
-element2.style.margin = margin();
-element2.style.background = color()
-element2.style.transform = rotate()
+element1.setAttribute("style","width:260px; height:260px; font-size:25px; padding:25px; margin-top:10px; overflow:hidden; box-shadow: 10px 10px 5px 0px rgba(0, 0, 0, 0.4)");
+element1.style.margin = margin();
+element1.style.background = color()
+element1.style.transform = rotate()
 
 element1.appendChild(element2);
- container2.insertAdjacentElement('beforeend',element1);
+ container2.insertAdjacentElement("afterbegin",element1);
 
  element1.addEventListener("mouseenter", function(){
     element1.style.transform = "scale(1.1)";
@@ -51,23 +52,28 @@ element1.appendChild(element2);
 
  document.getElementById("note-text").value = "";
 
+ checkIcon.addEventListener('click', ()=>{           //just tried arrow function :)
+    typeNote();
+    container3.style.display="none";
+ })
+
 }
 
 function margin(){
-    var random_margin = ['-15px', '1px', '5px','15px','10px', '20px'];
-    // to generate a random number 
+    var random_margin = ['-15px', '1px', '5px','15px','10px', '20px'];  // to generate a random number
+     
     return random_margin[Math.floor(Math.random()*random_margin.length)];
    
 }
 
 function rotate(){
-    var random_rotate = ['rotate(3deg)','rotate(1deg)','rotate(-1deg)','rotate.-3deg)','rotate(-5deg)','rotate(-10deg)'];
-    // to generate a rotation degree
+    var random_rotate = ['rotate(3deg)','rotate(1deg)','rotate(-1deg)','rotate.-3deg)','rotate(-5deg)','rotate(-10deg)'];  // to generate a rotation degree
+    
     return random_rotate[Math.floor(Math.random()*random_rotate.length)];
 }
 
 function color(){
-    var random_color = ['#c2ff3d','#ff3de8','#3dc2ff','#04e022','#bc83e6','#ebb328'];
+    var random_color = ['#c2ff3d','#ff3de8','#3dc2ff','#04e022','#bc83e6','#ebb328']; 
     if(i>random_color.length-1){
         i=0;
     }
