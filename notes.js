704ae9614a6640121e94611dbdb82dd7efe3ceb0
ZN <154010547+Hei-sen-bergg@users.noreply.github.com>
2024-03-30 22:03:53 +0530

    Update notes.js

--- notes.js
+++ notes.js
@@ -1,9 +1,9 @@
-var container2 = document.getElementsByClassName("container2")[0];
-var container3 = document.getElementsByClassName("container3")[0];
-var checkIcon = document.getElementById("check-icon");
-var xIcon = document.getElementById("x-icon");
+let container2 = document.getElementsByClassName("container2")[0];
+let container3 = document.getElementsByClassName("container3")[0];
+let checkIcon = document.getElementById("check-icon");
+let xIcon = document.getElementById("x-icon");
 
-var i=0;
+let i=0;
 
 xIcon.addEventListener("click", function(){
     typeNote();
@@ -24,9 +24,9 @@ function typeNote(){
 }
 
 function createNote(){                        //input text for notes
-    var element1 = document.createElement('div');
- var noteText = document.getElementById("note-text").value;  
- var element2 = document.createElement('h1');
+    let element1 = document.createElement('div');
+ let noteText = document.getElementById("note-text").value;  
+ let element2 = document.createElement('h1');
 
  element2.innerHTML = noteText;
 
@@ -60,20 +60,20 @@ element1.appendChild(element2);
 }
 
 function margin(){
-    var random_margin = ['-15px', '1px', '5px','15px','10px', '20px'];  // to generate a random number
+    let random_margin = ['-15px', '1px', '5px','15px','10px', '20px'];  // to generate a random number
      
     return random_margin[Math.floor(Math.random()*random_margin.length)];
    
 }
 
 function rotate(){
-    var random_rotate = ['rotate(3deg)','rotate(1deg)','rotate(-1deg)','rotate.-3deg)','rotate(-5deg)','rotate(-10deg)'];  // to generate a rotation degree
+    let random_rotate = ['rotate(3deg)','rotate(1deg)','rotate(-1deg)','rotate.-3deg)','rotate(-5deg)','rotate(-10deg)'];  // to generate a rotation degree
     
     return random_rotate[Math.floor(Math.random()*random_rotate.length)];
 }
 
 function color(){
-    var random_color = ['#c2ff3d','#ff3de8','#3dc2ff','#04e022','#bc83e6','#ebb328']; 
+    let random_color = ['#c2ff3d','#ff3de8','#3dc2ff','#04e022','#bc83e6','#ebb328']; 
     if(i>random_color.length-1){
         i=0;
     }
